441c825a66a70b5851d54357a8116f5f6969e1eb
feijiumeng_zn <feijiumeng@163.com>
2024-05-24 08:06:49 +0800

    Optional

---
+++ use-jdk/src/main/java/javatest/security/SecureRandomTest.java
@@ -0,0 +1,10 @@
+package javatest.security;
+
+import java.security.SecureRandom;
+
+public class SecureRandomTest {
+
+    public static void main(String[] args) {
+        SecureRandom secureRandom = new SecureRandom();
+    }
+}
